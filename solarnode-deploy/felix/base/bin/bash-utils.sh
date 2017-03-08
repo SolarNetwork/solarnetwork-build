8cf6e880dfe649eaac0f843a516a0217d9e0e403
Matt Magoffin <matt@solarnetwork.net>
2017-03-08 14:05:18 +1300

    Add build for a Felix 5.6.2 based distribution.

---
+++ solarnode-deploy/felix/base/bin/bash-utils.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sn-log-path() {
+	echo /run/solar/log/solarnode.log
+}
+
+sn-log-tail() {
+	tail "$@" `sn-log-path`
+}
+
+sn-ctl() {
+	sudo systemctl $1 solarnode
+}
+
+sn-stop() {
+	sn-ctl stop
+}
+
+sn-start() {
+	sn-ctl start
+}
+
+sn-restart() {
+	sn-ctl restart
+}
+
+sn-status() {
+	sn-ctl status
+}
+
